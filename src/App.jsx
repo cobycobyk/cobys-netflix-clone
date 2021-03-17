e15e98bf00334883ce74ced6ac686212564f207b
cobycobyk <coby@robo3d.com>
2021-03-17 14:24:53 -0600

    add banner and nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
 import "./App.css";
+import Banner from "./components/Banner/Banner";
+import Nav from "./components/Nav/Nav";
 import Row from "./components/Row/Row";
+import requests from "./requests";
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>home</h1>
-      <Row title="NETFLIX ORIGINALS" />
-      <Row title="Trending Now" />
+    <div className="app">
+      <Nav />
+      <Banner />
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchURL={requests.fetchNetflixOriginals}
+        isLargeRow
+      />
+      <Row title="Trending Now" fetchURL={requests.fetchTrending} />
+      <Row title="Top Rated" fetchURL={requests.fetchTrending} />
+      <Row title="Action Movies" fetchURL={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchURL={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchURL={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchURL={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchURL={requests.fetchDocumentaries} />
     </div>
   );
 }
--- src/components/Row/Row.jsx
+++ src/components/Row/Row.jsx
@@ -1,10 +1,36 @@
-import React from "react";
+import { useState, useEffect } from "react";
 import "./Row.css";
+import axios from "../../axios";
 
-export default function Row() {
+const baseURL = "https://image.tmdb.org/t/p/original/";
+
+export default function Row({ title, fetchURL, isLargeRow }) {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(fetchURL);
+      setMovies(request.data.results);
+      return request;
+    }
+    fetchData();
+  }, [fetchURL]);
+
   return (
     <div className="row">
-      <h1>row</h1>
+      <h2>{title}</h2>
+      <div className="row__posters">
+        {movies.map((movie) => (
+          <img
+            key={movie.id}
+            className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+            src={`${baseURL}${
+              isLargeRow ? movie.poster_path : movie.backdrop_path
+            }`}
+            alt={movie.name}
+          />
+        ))}
+      </div>
     </div>
   );
 }
